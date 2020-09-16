cbca32169386067d574c297cc00ba194376a9806
Cristian <cristian123105@gmail.com>
2020-09-16 16:12:19 -0300

    chore: importing all modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component'
+
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav'
 @NgModule({
+  // Declaração de componentes, diretivas e pipes
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent
   ],
+  // Importações de outros modulos, modulo seu ou de uma lib externa
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule
   ],
+  /* Componentes, diretivas e pipes que serão exportados para outros modulos
+  No app.module nada é exportado e sim importado por ser o modulo principal */
+  exports: [],
+  // Declaração de services
   providers: [],
+  // Define o componente que será carregado no modulo
   bootstrap: [AppComponent]
 })
 export class AppModule { }
