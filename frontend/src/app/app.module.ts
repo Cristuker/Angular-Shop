c0e3e077a1a49cc83a6a0e6948cf380005acf351
Cristian <cristian123105@gmail.com>
2020-09-18 14:31:13 -0300

    refactor: add red directive

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list'
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './components/template/header/header.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { FooterComponent } from './components/template/footer/footer.component';
-import { NavComponent } from './components/template/nav/nav.component'
-
-import { MatListModule } from '@angular/material/list'
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { HomeComponent } from './views/home/home.component'
+import { HeaderComponent,FooterComponent,NavComponent } from './components';
+import { HomeComponent, ProductCrudComponent } from './views/';
+import { RedDirective } from './directives/red.directive';
 @NgModule({
   // Declaração de componentes, diretivas e pipes
   declarations: [
@@ -20,7 +20,9 @@ import { HomeComponent } from './views/home/home.component'
     HeaderComponent,
     FooterComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    ProductCrudComponent,
+    RedDirective
   ],
   // Importações de outros modulos, modulo seu ou de uma lib externa
   imports: [
@@ -29,7 +31,8 @@ import { HomeComponent } from './views/home/home.component'
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   /* Componentes, diretivas e pipes que serão exportados para outros modulos
   No app.module nada é exportado e sim importado por ser o modulo principal */
